d2f678e5990cfc8a39a2f3e3053296149d799c52
SXRDXR <sardorboynazarv@gmail.com>
2025-12-30 15:01:53 +0500

    added new font for czech lang

--- src/i18n.ts
+++ src/i18n.ts
@@ -25,4 +25,11 @@ i18n
     }
   });
 
+i18n.on('languageChanged', (lng) => {
+  document.documentElement.lang = lng;
+});
+
+// Set initial lang attribute
+document.documentElement.lang = i18n.language || 'ru';
+
 export default i18n;
